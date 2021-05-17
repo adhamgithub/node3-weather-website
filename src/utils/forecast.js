147c1ea55088b8a308c3a82eae75edf403046e01
ABD EL HAMEED EZZAT Adham OBS/CMI <adham.abdelhameedezzat@orange.com>
2021-05-17 11:01:14 +0200

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=864e35e3d7acf8d33ade11a8d77eae4c&units=metric'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather services', undefined);
+        } else if (JSON.stringify(body.cod).startsWith('4')) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, {
+                temp: body.main.temp,
+                name: body.name
+            });
+        }
+    });
+}
+
+module.exports = forecast;
